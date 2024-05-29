a270fb200a8173fd52c9e79bd82714ec64d1701e
walid bouwachma <oualidd@walids-MacBook-Air.local>
2024-05-29 19:08:31 +0100

    ui chat finish

--- front/views/Home.js
+++ front/views/Home.js
@@ -17,16 +17,16 @@ export class Home extends Abstract {
     async getHtml() {
         return `
             <div class="container">
+                <div class="overlay">
+                </div>
                 <div class="mini-container">
-                <ul class="list">
-                    <li class="big-li" id="active"><a href="">PLAY</a></li>
-                    <li class="big-li"><a href="">LEADERBOARD</a></li>
-                    <li class="big-li"><a href="">CAREER</a></li>
-                    <li class="big-li"><a href="">CHAT</a></li>
-                    <li class="small-li"><a href="">SETTINGS</a></li>
-                    <li class="small-li"><a href="">SOCIAL</a></li>
-                    <li class="small-li"><a href="">LOGOUT</a></li>
-                </ul>
+                    <ul class="list">
+                        <li class="big-li" id="active"><a href="">PLAY With Bot</a></li>
+                        <li class="big-li"><a href="">Search</a></li>
+                        <li class="big-li"><a href="">CHAT</a></li>
+                        <li class="big-li"><a href="">SETTINGS</a></li>
+                        <li class="big-li"><a href="">LOGOUT</a></li>
+                    </ul>
                 </div>
                 <div class="mini-container-2">
                         <div class="upper"></div>
