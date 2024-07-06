1806434ca22706428a20008ef0a8874e3d63965b
walid bouwachma <oualidd@walids-MacBook-Air.local>
2024-07-06 01:35:32 +0100

    hot reloading

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,25 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './index.js',  // Adjust the entry point based on your project structure
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')  // Adjust the output directory as needed
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-            // Add more loaders if necessary for other file types (e.g., CSS, images)
-        ]
-    }
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    watchContentBase: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // Add other loaders as needed
+    ],
+  },
 };
